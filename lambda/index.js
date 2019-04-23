83dd775405fd1609b2e9dcb34b4fbebe8ad92859
Luke Ness <gausnes@gmail.com>
2019-04-22 23:15:50 -0500

    Creating SmartApp Library and WebHook Server

--- lambda/index.js
+++ lambda/index.js
@@ -1,41 +1,5 @@
-const SmartApp = require('@smartthings/smartapp');
-const DynamoDBContextStore = require('@smartthings/dynamodb-context-store');
+const smartApp = require('smartapp-lib');
 const qs = require('qs');
-const request = require('request-promise-native');
-
-const subscriptionHandlerName = 'subscriptionHandler';
-
-const smartapp = new SmartApp()
-    .contextStore(new DynamoDBContextStore('us-east-2', 'smartthings-slack-context-store'))
-    .configureI18n()
-    .page('mainPage', (context, page, configData) => {
-        page.section('lights', section => {
-            section.deviceSetting('lights').capabilities(['switch']).multiple(true).permissions('rx');
-        });
-    })
-    .updated( async (context, updateData) => {
-        console.log('updated', JSON.stringify(updateData));
-        await context.api.subscriptions.unsubscribeAll();
-        console.log('updated', 'unsubscribeAll() executed');
-        context.api.subscriptions.subscribeToDevices(context.config.lights, 'switch', 'switch', subscriptionHandlerName);
-    })
-    .subscribedEventHandler(subscriptionHandlerName, async (context, deviceEvent) => {
-        console.log(subscriptionHandlerName, 'context', context);
-        console.log(subscriptionHandlerName, 'event', deviceEvent);
-
-        const device = await context.api.devices.get(deviceEvent.deviceId);
-        console.log(subscriptionHandlerName, 'device', device);
-
-        const slackWebHookUrl = 'https://hooks.slack.com/services/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';
-        const payload = {"text": `${device.label} emitted ${deviceEvent.value} for ${deviceEvent.attribute}`};
-        const response = await request({
-            method: 'POST',
-            uri: slackWebHookUrl,
-            json: true,
-            body: payload
-        });
-        console.log(subscriptionHandlerName, 'slack-webhook', response);
-    });
 
 exports.handler = async (event, context, callback) => {
     if (event.resource === '/SmartThings-Slack') {
@@ -45,7 +9,7 @@ exports.handler = async (event, context, callback) => {
         const command = text[1];
 
         const installedSmartAppId = event.queryStringParameters.installedSmartAppId;
-        const smartAppContext = await smartapp.withContext(installedSmartAppId);
+        const smartAppContext = await smartApp.withContext(installedSmartAppId);
         console.log('request', 'context', smartAppContext);
         console.log('request', 'context-config', smartAppContext.config);
 
@@ -82,6 +46,6 @@ exports.handler = async (event, context, callback) => {
             })
         });
     } else {
-        smartapp.handleLambdaCallback(event, context, callback);
+        smartApp.handleLambdaCallback(event, context, callback);
     }
 };
